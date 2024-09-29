78b867d66e96e43f3f6b9238b30b72607da36a14
Nunki <harlockcaptain30@gmail.com>
2024-09-29 13:57:37 +0300

    Base files

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add interactivity for forms or dynamic content if needed
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Mesajul tău a fost trimis!');
+});
